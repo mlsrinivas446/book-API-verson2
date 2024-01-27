17f5124939b1daa6eb25e854335c60ef6e07d57c
mlsrinivas446 <mlsrinivas2233@gmail.com>
2024-01-27 05:43:13 +0000

    added put, delete and to get author books

--- index.js
+++ index.js
@@ -91,3 +91,60 @@ app.post('/books/', async (request, response) => {
   const bookId = dbResponse.lastID //lastID give last id of book
   response.send({book_id: bookId})
 })
+
+app.put('/books/:bookId/', async (request, response) => {
+  const {bookId} = request.params
+  const bookDetails = request.body
+  const {
+    title,
+    authorId,
+    rating,
+    ratingCount,
+    reviewCount,
+    description,
+    pages,
+    dateOfPublication,
+    editionLanguage,
+    price,
+    onlineStores,
+  } = bookDetails
+
+  const updateBookQuery = `
+    UPDATE
+      book
+    SET
+      title='${title}',
+      author_id=${authorId},
+      rating=${rating},
+      rating_count=${ratingCount},
+      review_count=${reviewCount},
+      description='${description}',
+      pages=${pages},
+      date_of_publication='${dateOfPublication}',
+      edition_language='${editionLanguage}',
+      price= ${price},
+      online_stores='${onlineStores}'
+    WHERE
+      book_id = ${bookId};`
+
+  const updateBook = await db.run(updateBookQuery)
+  response.send('Book successfully updated')
+})
+
+app.delete('/books/:bookId/', async (request, response) => {
+  const {bookId} = request.params
+  const deleteBookQuery = `
+    DELETE FROM
+        book
+    WHERE
+        book_id = ${bookId};`
+  const bookDeleted = await db.get(deleteBookQuery)
+  response.send('Book successfully deleted')
+})
+
+app.get('/authors/:authorId/books/', async (request, response) => {
+  const {authorId} = request.params
+  const getAuthorBooksQuery = `SELECT * FROM book WHERE author_id=${authorId};`
+  const getAuthorBooks = await db.all(getAuthorBooksQuery)
+  response.send(getAuthorBooks)
+})
